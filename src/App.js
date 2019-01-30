e0f9353f92ff9199c8ff55d5d05bff25cc30a25e
Berkan <berkanunal@gmail.com>
2019-01-30 12:51:32 +0300

    Add earchPhotos method

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import axios from "axios";
+
 import "./App.css";
+
 import ContentLayout from "./components/ContentLayout";
 import Header from "./components/Header";
 
 class App extends Component {
-  state = {};
+  state = {
+    params: {
+      collections: "",
+      query: ""
+    },
+    content: {}
+  };
+
+  searchPhotos = () => {
+    axios
+      .get("https://site.com/", {
+        params: {
+          client_id:
+            "bdafd9c0457f994a91c042f1445ff058ee00fbbe745f642d5037ba1dd170c157",
+          query: this.state.params.query,
+          collections: this.state.params.collections
+        }
+      })
+      .then(response => {
+        this.setState({
+          content: response.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
   render() {
     return (
       <Router>
