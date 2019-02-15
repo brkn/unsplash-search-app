3c0f0cc245e7d59a6d9bb941603e46d08c8271f6
Berkan Unal <Berkanunal@gmail.com>
2019-02-15 18:38:58 +0300

    Add css for profile info, fix classnames of profileInfo component

--- src/components/Content/Modal/ProfileInfo/ProfileInfo.js
+++ src/components/Content/Modal/ProfileInfo/ProfileInfo.js
@@ -6,10 +6,10 @@ class ProfileInfo extends Component {
   render() {
     const { user } = this.props;
     return (
-      <div className="profile-info-wrapper">
-        <img src={user.profile_image.medium} alt={user.username} />
-        <div className="name">{user.name}</div>
-        <a className="username" href={user.links.html}>@{user.username}</a>
+      <div className="profile-wrapper">
+        <img className="profile-image" src={user.profile_image.medium} alt={user.username} />
+        <div className="profile-name">{user.name}</div>
+        <a className="profile-username" href={user.links.html}>@{user.username}</a>
       </div>
     );
   }
